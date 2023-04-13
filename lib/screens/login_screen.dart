edb437daeeeae8081cbafa9cc6751774b3e280c5
Zarif <2502.zarif@gmail.com>
2023-04-13 06:20:05 +0600

    Extract validator functions Student ID validator Password validator

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -59,18 +59,7 @@ class _LoginScreenState extends State<LoginScreen> {
         suffix: Text("@student.cuet.ac.bd"),
       ),
       keyboardType: TextInputType.number,
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return "Please enter your Student ID";
-        }
-        if (int.tryParse(value) == null) {
-          return "Student ID must contain digits only";
-        }
-        if (value.length != 7) {
-          return "Student ID must be 7 digits long";
-        }
-        return null;
-      },
+      validator: studentIdValidator,
     );
   }
 
@@ -85,12 +74,27 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       obscureText: true,
       keyboardType: TextInputType.visiblePassword,
-      validator: (value) {
-        if (value == null || value.length < 6) {
-          return "Password must be at least 6 characters long";
-        }
-        return null;
-      },
+      validator: passwordValidator,
     );
+  }
+
+  String? studentIdValidator(value) {
+    if (value == null || value.isEmpty) {
+      return "Please enter your Student ID";
+    }
+    if (int.tryParse(value) == null) {
+      return "Student ID must contain digits only";
+    }
+    if (value.length != 7) {
+      return "Student ID must be 7 digits long";
+    }
+    return null;
+  }
+
+  String? passwordValidator(value) {
+    if (value == null || value.length < 6) {
+      return "Password must be at least 6 characters long";
+    }
+    return null;
   }
 }
